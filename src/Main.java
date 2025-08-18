28bc64a4be068cebec8713ac5dc13f83a8709cc3
Ella Sharmaine N. Abeso <eabeso63122@liceo.edu.ph>
2025-08-18 15:12:12 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 
 public static void main(String[] args) {
     GradeCalculator gradeCalculator = new GradeCalculator(); //iyang instance/reference
-// the inputs
+    // the inputs
     String studentName = "John Smith";
     double average = gradeCalculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
     String letterGrade = gradeCalculator.getLetterGrade(average);
 
-//iget sa class
+    //iget sa class
     gradeCalculator.displayResult(studentName, average);
     gradeCalculator.displayResult(studentName, average, letterGrade);
 }
