96eff8482588ba842a1a735eecb60d3204103dca
Ella Sharmaine N. Abeso <eabeso63122@liceo.edu.ph>
2025-09-02 12:23:36 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        LibraryManager manager = new LibraryManager();
 
-        System.out.println("Test Branch");
+        manager.showBooks();
+        manager.addBook();
+        manager.removeBook();
     }
 }
