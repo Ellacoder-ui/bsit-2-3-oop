0fb55c6043840105e42451bcb6b616d180fa8d86
Ella Sharmaine N. Abeso <eabeso63122@liceo.edu.ph>
2025-09-23 09:56:23 +0800

    Dog.java

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+class Dog extends Pet {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " says: Woof! Woof!");
+    }
+}
